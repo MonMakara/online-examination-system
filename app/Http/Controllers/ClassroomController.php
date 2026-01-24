5c14d7d37eb9fa4975cf1d3dfcaa66bdf9761391
MakaraCoder <makaramun2@gmail.com>
2026-01-24 14:54:38 +0700

    fix in for teacher page

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -16,6 +16,18 @@ class ClassroomController extends Controller
 
         $exams = $class->exams()->latest()->get();
 
-        return view('teacher.classes.show', compact('class', 'students', 'exams'));
+        // Calculate Stats
+        $activeExamsCount = $class->exams()
+            ->where(function ($query) {
+                $query->whereNull('closed_at')
+                      ->orWhere('closed_at', '>', now());
+            })
+            ->count();
+
+        $examIds = $exams->pluck('id');
+        $averageScore = \App\Models\Result::whereIn('exam_id', $examIds)->avg('score');
+        $classAverage = $averageScore ? round($averageScore, 1) . '%' : 'N/A';
+
+        return view('teacher.classes.show', compact('class', 'students', 'exams', 'activeExamsCount', 'classAverage'));
     }
 }
